ae9c7c1cf1708147d5c9793cafe20e78bc5ea104
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-27 14:53:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 int hours, minutes, seconds;
 
 int validate() {
-    if (hours>24 || hours<0) {
-
+    if (hours>24 || hours<0 || minutes>60 || minutes<0 || seconds>60 || seconds<0) {
+        return 0;
     }
+    return 1;
 }
 
 int main() {
